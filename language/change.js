90df1c07b35b3aee2d82bb7b545dce1604a223c5
chenchenalex <chenchenalex1@gmail.com>
2014-08-20 17:43:41 +0800

    first commit

---
+++ language/change.js
@@ -0,0 +1,7 @@
+var aws_lang = new Array();
+aws_lang['发现'] = '首页';
+aws_lang['话题广场'] = '名企';
+aws_lang['问题'] = '问答';
+aws_lang['文章'] = '首页';
+aws_lang['话题'] = '标签';
+aws_lang['发布文章'] = '发起人贩贴';
---
+++ language/change.php
@@ -0,0 +1,8 @@
+<?php
+$language['发现'] = '首页';
+$language['话题广场'] = '名企';
+$language['话题'] = '标签';
+$language['问题'] = '问答';
+$language['文章'] = '首页';
+$language['发布文章'] = '发起人贩贴';
+?>
